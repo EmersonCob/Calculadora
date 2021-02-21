6c96878da86cfe1e6dd19b9897b30323e4ae22cf
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-02-21 00:54:12 -0300

    Calculate
    Calculadora usando HTML, CSS e JavaScript.

---
+++ calculate.js
@@ -0,0 +1,41 @@
+// calculadora simples usando o switch com entrada de dados do usuario.
+
+
+
+
+document.getElementById("botao").value
+
+function calculate (number1, operate, number2) {
+    
+    let result = 0
+
+    number1 = document.querySelector(numero1);innerHTML;
+    operate = ("Informe o operador: ");
+    number2 = document.querySelector(numero2);innerHTML;
+
+
+    switch (operate) {
+        case '+':
+            result = Number(number1) + Number(number2)
+            break
+        case '-':
+            result = number1 - number2
+            break
+        case '*':
+            result = number1 * number2
+            break
+        case '/':
+            result = number1 / number2
+            break
+        default:
+             window.alert('Não implementado')
+             break
+        return 0;           
+    }
+
+    window.alert(`O resultado é: ${result}`)
+}
+
+calculate(numero1, operador, numero2)
+
+
